f70093bc04a8bfcb16ee79fe668b796fcb2d0a32
fazal-haroon <kharoon187@gmail.com>
2023-08-19 21:38:14 +0300

    some components added for exam portal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        FooterComponent,
+        SignupComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         MatButtonModule,
+        AppRoutingModule,
+        RouterOutlet,
     ],
     providers: [],
     bootstrap: [AppComponent]
