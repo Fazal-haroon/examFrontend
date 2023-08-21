4ba57eae1a37e49eca53c977663ba2f92c4d5bc6
fazal-haroon <kharoon187@gmail.com>
2023-08-21 22:38:30 +0300

    added sweetAlert for popup

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from "../../services/user.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import Swal from "sweetalert2";
 
 @Component({
     selector: 'app-signup',
@@ -30,30 +31,45 @@ export class SignupComponent implements OnInit {
 
     formSubmit() {
         if (this.signupForm.valid) {
-            alert('submitted');
+            // alert('submitted');
             const payload = this.signupForm.value; // Get form values
             this.userService.addUser(payload).subscribe(
                 data => {
                     // Handle success
                     console.log('User added successfully:', data);
                     // alert('success')
-                    this.snack.open('successfully created', 'ok', {
-                        duration: 3000
-                    })
+                    // this.snack.open('successfully created', 'ok', {
+                    //     duration: 3000
+                    // })
+                    Swal.fire('Success',
+                        'user is registered ' + data.id,
+                        'success');
                 },
                 error => {
                     // Handle error
                     console.error('Error adding user:', error);
                     // alert('something went wrong')
-                    this.snack.open('something went wrong', 'ok', {
-                        duration: 3000
+                    // this.snack.open('something went wrong', 'ok', {
+                    //     duration: 3000
+                    // })
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                        footer: '<a href="">Why do I have this issue?</a>'
                     })
                 }
             );
         } else {
             // alert('form is invalid');
-            this.snack.open("Form is invalid!!", 'ok', {
-                duration: 3000,
+            // this.snack.open("Form is invalid!!", 'ok', {
+            //     duration: 3000,
+            // })
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Form is invalid!',
+                footer: '<a href="">Why do I have this issue?</a>'
             })
         }
     }
