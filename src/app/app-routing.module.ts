af08ec4f3de9bab026c2980c7a7157b75b7eeb7e
fazal-haroon <kharoon187@gmail.com>
2023-11-04 13:45:06 +0300

    Admin UI and added new profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,41 @@ import {DashboardComponent} from "./pages/admin/dashboard/dashboard.component";
 import {UserDashboardComponent} from "./pages/user/user-dashboard/user-dashboard.component";
 import {AdminGuard} from "./services/admin.guard";
 import {NormalGuard} from "./services/normal.guard";
+import {ProfileComponent} from "./pages/profile/profile.component";
 
 const routes: Routes = [
-    {path: 'signup', component: SignupComponent, pathMatch: 'full',},
-    {path: '', component: HomeComponent, pathMatch: 'full',},
-    {path: 'login', component: LoginComponent, pathMatch: 'full',},
-    {path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard]},
-    {path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard]},
+    {
+        path: 'signup',
+        component: SignupComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'admin',
+        component: DashboardComponent,
+        children: [
+            {
+                path: 'profile',
+                component: ProfileComponent,
+            }
+        ],
+        canActivate: [AdminGuard]
+    },
+    {
+        path: 'user-dashboard',
+        component: UserDashboardComponent,
+        pathMatch: 'full',
+        canActivate: [NormalGuard]
+    },
 ];
 
 @NgModule({
